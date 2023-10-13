236cf51e2de64fdfbfafec8caf861c31ff70db67
developShin <blueskyroad@naver.com>
2023-10-13 16:14:29 +0900

    [DESIGN] : 🎨(sd): 설정 페이지 생성 공지사항 폰트변경 color red추가

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/MainActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.withallmyanimal.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_setting)
     }
 }
