154584f48aa7ec5dc8fdd5adc911460c9958db78
minji-0420 <pongiii17@gmail.com>
2023-10-13 17:07:07 +0900

    [DESIGN] 일지 상세 페이지 - 병원

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/DetailHospitalActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/DetailHospitalActivity.kt
@@ -7,6 +7,6 @@ import com.example.withallmyanimal.R
 class DetailHospitalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_hospital)
+        setContentView(R.layout.activity_detail_hospital)
     }
 }
