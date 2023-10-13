bd798c6b5ff3c61b195c44a594e3eb7ccb0104be
minji-0420 <pongiii17@gmail.com>
2023-10-13 20:16:36 +0900

    [DESIGN] 일지 상세 페이지 - 일상 UI 완성

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/detail/DetailBehaviorActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/detail/DetailBehaviorActivity.kt
@@ -1,12 +1,12 @@
-package com.kittyandpuppy.withallmyanimal
+package com.kittyandpuppy.withallmyanimal.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.withallmyanimal.R
 
-class DetailHospitalActivity : AppCompatActivity() {
+class DetailBehaviorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_hospital)
+        setContentView(R.layout.activity_detail_behavior)
     }
 }
--- app/src/main/java/com/kittyandpuppy/withallmyanimal/detail/DetailPetActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/detail/DetailPetActivity.kt
@@ -1,12 +1,12 @@
-package com.kittyandpuppy.withallmyanimal
+package com.kittyandpuppy.withallmyanimal.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.withallmyanimal.R
 
-class MainActivity : AppCompatActivity() {
+class DetailPetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail_pet)
     }
 }
