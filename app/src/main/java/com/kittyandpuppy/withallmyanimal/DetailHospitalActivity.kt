34f9b065e9905dc1ed312ca33c5d91fe1fb6d49f
minji-0420 <pongiii17@gmail.com>
2023-10-13 14:09:14 +0900

    [DESIGN] 일지 상세 페이지 추가

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/DetailHospitalActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/DetailHospitalActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.withallmyanimal.R
 
-class HomeHospitalActivity : AppCompatActivity() {
+class DetailHospitalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_hospital)
