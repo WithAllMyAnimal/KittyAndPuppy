515c3c98195a422bc85eae34a35facb9b87cc351
minji-0420 <pongiii17@gmail.com>
2023-10-17 16:36:24 +0900

    [FEAT] MyPage - firebase 연동 중

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/write/MypageBehavior.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/write/MypageBehavior.kt
@@ -2,11 +2,38 @@ package com.kittyandpuppy.withallmyanimal.write
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import coil.load
 import com.kittyandpuppy.withallmyanimal.R
+import com.kittyandpuppy.withallmyanimal.databinding.ActivityMypageBehaviorBinding
+import com.kittyandpuppy.withallmyanimal.firebase.FBAuth
+import com.kittyandpuppy.withallmyanimal.firebase.FBRef
 
 class MypageBehavior : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMypageBehaviorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage_behavior)
+        setContentView(binding.root)
+
+        binding.btnMypageBehaviorSave.setOnClickListener {
+            val title = binding.etvMypageBehaviorTitle.text.toString()
+            val content = binding.etvMypageBehavior.text.toString()
+            val tag = binding.etvMypageBehaviorTag.text.toString()
+            val review = binding.etvMypageBehaviorReview.text.toString()
+            val uid = FBAuth.getUid()
+            val time = FBAuth.getTime()
+
+            val key = FBRef.boardRef.push().key.toString()
+
+            FBRef.boardRef
+                .child(key)
+                .setValue(Behavior(title, content, tag, review, uid, time))
+
+            Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
