50e8fa8622d03512ecf4d048bc282c666712bb6a
developShin <blueskyroad@naver.com>
2023-10-17 16:08:12 +0900

    [FEAT] : ⚙️(Setting activity 에서 Fragment로 이동 기능 구현): 이동 기능 구현

---
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/SettingPasswordFragment.kt
@@ -0,0 +1,24 @@
+package com.kittyandpuppy.withallmyanimal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+
+class SettingPasswordFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, android.R.style.Theme_Black_NoTitleBar_Fullscreen)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_setting_passworddialog, container, false)
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        return view
+    }
+}
