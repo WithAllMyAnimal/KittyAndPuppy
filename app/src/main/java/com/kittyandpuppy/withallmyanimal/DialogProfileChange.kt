03b6672221615be25d3833e765fb32de0c7f3d6b
developShin <blueskyroad@naver.com>
2023-10-19 20:50:44 +0900

    [FEAT] : ⚙️(Setting activity 에서 Fragment로 이동 기능 구현):

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/DialogProfileChange.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/DialogProfileChange.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kittyandpuppy.withallmyanimal.databinding.FragmentDialogProfilechangeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +14,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingPassworddialogFragment.newInstance] factory method to
+ * Use the [dialog_profilechange.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingPassworddialogFragment : Fragment() {
+class DialogProfileChange : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +36,9 @@ class SettingPassworddialogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting_passworddialog, container, false)
+        return inflater.inflate(R.layout.fragment_dialog_profilechange, container, false)
     }
+
 
     companion object {
         /**
@@ -44,12 +47,12 @@ class SettingPassworddialogFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingPassworddialogFragment.
+         * @return A new instance of fragment dialog_profilechange.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingPassworddialogFragment().apply {
+            DialogProfileChange().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
