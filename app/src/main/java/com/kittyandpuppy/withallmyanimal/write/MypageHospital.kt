5a1194904aefb842dc0cf04e61b10181f35d88e3
minji-0420 <pongiii17@gmail.com>
2023-10-19 11:28:39 +0900

    [FEAT] 이미지 업로드 구현 - fb storage 사용, 갤러리 접근 허용 추가 / util로 공통 코드 빼기 완료 / 사진 1장 업로드로 변경

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/write/MypageHospital.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/write/MypageHospital.kt
@@ -1,17 +1,32 @@
 package com.kittyandpuppy.withallmyanimal.write
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import com.kittyandpuppy.withallmyanimal.R
 import com.kittyandpuppy.withallmyanimal.databinding.ActivityMypageHospitalBinding
 import com.kittyandpuppy.withallmyanimal.firebase.FBAuth
 import com.kittyandpuppy.withallmyanimal.firebase.FBRef
+import com.kittyandpuppy.withallmyanimal.firebase.ImageUtils
 
 class MypageHospital : AppCompatActivity() {
 
-    private val binding  : ActivityMypageHospitalBinding by lazy { ActivityMypageHospitalBinding.inflate(layoutInflater) }
+    private val binding: ActivityMypageHospitalBinding by lazy {
+        ActivityMypageHospitalBinding.inflate(
+            layoutInflater
+        )
+    }
 
+    private val PICK_IMAGE_REQUEST = 0
+    private val PERMISSION_REQUEST_CODE = 1
+
+    private var isImageUpload = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -30,9 +45,65 @@ class MypageHospital : AppCompatActivity() {
 
             FBRef.boardRef
                 .child(key)
-                .setValue(Hospital("Hospital", content, date, location, price, tag, time, title, uid))
+                .setValue(
+                    Hospital(
+                        "Hospital",
+                        content,
+                        date,
+                        location,
+                        price,
+                        tag,
+                        time,
+                        title,
+                        uid
+                    )
+                )
 
             Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
+
+            if (isImageUpload) {
+                ImageUtils.imageUpload(this, binding.ivMypageHospitalPictureLeft, key)
+            }
+            finish()
+        }
+        binding.ivMypageHospitalPictureLeft.setOnClickListener {
+            isImageUpload = true
+            ImageUtils.openGallery(this, PICK_IMAGE_REQUEST)
+        }
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 부여된 경우 갤러리 열기
+                ImageUtils.openGallery(this, PICK_IMAGE_REQUEST)
+            } else {
+                AlertDialog.Builder(this)
+                    .setMessage("갤러리 접근 권한이 거부되었습니다. 설정에서 권한을 허용해주세요.")
+                    .setPositiveButton("설정으로 이동") { _, _ ->
+                        // 설정 화면으로 이동하여 권한을 허용할 수 있도록 유도
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", this.packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    }
+                    .setNegativeButton("취소") { _, _ -> }
+                    .show()
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            binding.ivMypageHospitalPictureLeft.setImageURI(data.data)
         }
     }
 }
