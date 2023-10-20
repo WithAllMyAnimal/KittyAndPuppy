bbad01321964c85b2e03e4515d08f8bb3384adaf
developShin <blueskyroad@naver.com>
2023-10-20 09:33:46 +0900

    [DESIGN] : 🎨(Setting activity 에서 Fragment로 이동 기능 구현): dialogpage 사이즈 조절

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/SettingLogoutFragment.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/SettingLogoutFragment.kt
@@ -1,19 +1,41 @@
 package com.kittyandpuppy.withallmyanimal
 
+import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
+import com.kittyandpuppy.withallmyanimal.databinding.FragmentSettingLogoutBinding
 
 class SettingLogoutFragment : DialogFragment() {
+    private var _binding: FragmentSettingLogoutBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_setting_logout, container, false)
-        return view
+        _binding = FragmentSettingLogoutBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //다이얼로그 size
+        val windowManager =
+            requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val deviceWidth = size.x
+        val deviceHeight = size.y
+        params?.width = (deviceWidth * 0.9).toInt()
+        params?.height = (deviceHeight * 0.35).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
     }
 }
--- app/src/main/java/com/kittyandpuppy/withallmyanimal/SettingPasswordDialogFragment.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/SettingPasswordDialogFragment.kt
@@ -1,7 +1,10 @@
+import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.kittyandpuppy.withallmyanimal.R
 
@@ -13,5 +16,20 @@ class SettingPasswordDialogFragment : DialogFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_setting_passworddialog, container, false)
         return view
+    }
+    override fun onResume() {
+        super.onResume()
+        //다이얼로그 size
+        val windowManager =
+            requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val deviceWidth = size.x
+        val deviceHeight = size.y
+        params?.width = (deviceWidth * 0.95).toInt()
+        params?.height = (deviceHeight * 0.6).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
     }
 }
