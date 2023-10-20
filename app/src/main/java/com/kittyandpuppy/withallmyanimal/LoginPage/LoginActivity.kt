4272995c01b7b54d04710686e6991a3a26930b20
wjdehrbs <rhtmxhavus@naver.com>
2023-10-20 17:57:04 +0900

    [FEAT] 로그인할때 프로필이 이미있으면 메인으로 없으면 프로필 설정창으로 이동 기능 추가

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/LoginPage/LoginActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/LoginPage/LoginActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.kittyandpuppy.withallmyanimal.MainActivity
 import com.kittyandpuppy.withallmyanimal.databinding.ActivityLoginBinding
@@ -15,9 +19,9 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
     private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         auth = Firebase.auth
 
         setContentView(binding.root)
@@ -29,25 +33,40 @@ class LoginActivity : AppCompatActivity() {
             auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        val intent = Intent(this, DogAndCatAddActivity::class.java)
-                        intent.flags =
-                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        startActivity(intent)
+                        val userId = auth.currentUser?.uid ?: return@addOnCompleteListener
+                        val database = FirebaseDatabase.getInstance()
 
-                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        database.getReference("users").child(userId).child("profile")
+                            .addListenerForSingleValueEvent(object : ValueEventListener {
+                                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                    val petName =
+                                        dataSnapshot.child("userIdname").getValue(String::class.java)
+
+                                    if (petName != null) {
+                                        startActivity(
+                                            Intent(this@LoginActivity, MainActivity::class.java).apply {
+                                                flags =
+                                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                            }
+                                        )
+                                    } else {
+                                        startActivity(
+                                            Intent(this@LoginActivity, DogAndCatAddActivity::class.java).apply {
+                                                flags =
+                                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                            }
+                                        )
+                                    }
+
+                                    Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                                }
+
+                                override fun onCancelled(databaseError: DatabaseError) {}
+                            })
                     } else {
                         Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
                     }
-
                 }
-        }
-        binding.btnLoginSignup.setOnClickListener {
-            val signupFragment = LoginAddItemFragment()
-            signupFragment.show(supportFragmentManager, "signUpDialog")
-        }
-        binding.btnLoginResetPassword.setOnClickListener {
-            val findPasswordFragment = FindPwFragment()
-            findPasswordFragment.show(supportFragmentManager, "findPasswordDialog")
         }
     }
 }
