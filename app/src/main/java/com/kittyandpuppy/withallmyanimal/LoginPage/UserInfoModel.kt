bcf480cc467d21870e0d7d304c7a313057c92834
wjdehrbs <rhtmxhavus@naver.com>
2023-10-20 14:43:07 +0900

    [FEAT] 데이터 모델 추가

---
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/LoginPage/UserInfoModel.kt
@@ -0,0 +1,13 @@
+package com.kittyandpuppy.withallmyanimal.LoginPage
+
+data class UserInfoModel(
+    val email : String? = "",
+    val profileImageUrl : String? = "",
+    val nickName : String? = "",
+    val petName : String? = "",
+    val birth : String? = "",
+    val dogcat : String? = "",
+    val breed : String? = "",
+    val statusMessage : String? = "",
+    val uid : String? = ""
+)
