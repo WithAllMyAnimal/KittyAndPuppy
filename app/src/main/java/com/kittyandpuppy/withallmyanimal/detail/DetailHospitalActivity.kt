f11f15a5b78d2df94aca9148f04b06108d3cfe77
minji-0420 <pongiii17@gmail.com>
2023-10-24 16:45:18 +0900

    [FEATURE] DetailHospitalActivity에 bind 중

--- app/src/main/java/com/kittyandpuppy/withallmyanimal/detail/DetailHospitalActivity.kt
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/detail/DetailHospitalActivity.kt
@@ -3,10 +3,15 @@ package com.kittyandpuppy.withallmyanimal.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kittyandpuppy.withallmyanimal.R
+import com.kittyandpuppy.withallmyanimal.databinding.ActivityDetailHospitalBinding
 
 class DetailHospitalActivity : AppCompatActivity() {
+
+    private val binding : ActivityDetailHospitalBinding by lazy {
+        ActivityDetailHospitalBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_hospital)
+        setContentView(binding.root)
     }
 }
