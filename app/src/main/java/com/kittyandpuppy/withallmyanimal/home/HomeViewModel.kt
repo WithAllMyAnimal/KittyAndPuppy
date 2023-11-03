0fcf7ccbd64bc6e1a7235a31153d35686bcf7ee1
minji-0420 <pongiii17@gmail.com>
2023-11-04 00:21:31 +0900

    [FIX] 이미지 수정 시 홈피드 즉각 반영 수정 중 완료 / 홈피드 -> 마이피드 -> 홈피드 돌아왔을 때 항목 다 없어지는 문제 해결해야함.......

---
+++ app/src/main/java/com/kittyandpuppy/withallmyanimal/home/HomeViewModel.kt
@@ -0,0 +1,48 @@
+package com.kittyandpuppy.withallmyanimal.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.kittyandpuppy.withallmyanimal.write.BaseModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _boardList = MutableLiveData<List<BaseModel>>()
+    val boardList: LiveData<List<BaseModel>> = _boardList
+
+    init {
+        loadBoardList()
+    }
+
+    private fun loadBoardList() {
+        val databaseRef = FirebaseDatabase.getInstance().getReference("boards")
+        databaseRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val items = snapshot.children.map { child ->
+                    child.getValue(BaseModel::class.java)!!
+                }
+                _boardList.value = items
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+
+    fun getImageUrl(key: String): LiveData<String> {
+        val imageUrlLiveData = MutableLiveData<String>()
+        val imageRef = FirebaseDatabase.getInstance().getReference("${key}.png")
+        imageRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val url = snapshot.getValue(String::class.java)
+                imageUrlLiveData.value = url ?: ""
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+        return imageUrlLiveData
+    }
+}
