1ac4af80ec61bccd1234245dc59b848264a233af
minji-0420 <pongiii17@gmail.com>
2023-11-14 19:43:30 +0900

    build.gradle.kts 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kittyandpuppy.withallmyanimal"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
